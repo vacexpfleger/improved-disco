678ab12622649db00a061e9fc98fbe0e96bd0346
Václav Pfleger <58568137+vacexpfleger@users.noreply.github.com>
2021-12-16 11:17:57 +0100

    01

---
+++ main.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter import filedialog
+import tkinter as tk
+from pygame import mixer
+
+
+class Player:
+    def __init__(self, window):
+        window.geometry("200x200")
+        window.title("Přehrávač")
+
+        self.play_restart = tk.StringVar()
+        self.pause_resume = tk.StringVar()
+        self.play_restart.set("Play")
+        self.pause_resume.set("Pause")
+
+        load_button = Button(window, text="Load", width=10, font=("Arial",12), command=self.load)
+        load_button.place(x=100, y=40, anchor="center")
+        play_button = Button(window, textvariable=self.play_restart, width=10, font=("Arial", 12), command=self.play)
+        play_button.place(x=100, y=80, anchor="center")
+        load_button = Button(window, textvariable=self.pause_resume, width=10, font=("Arial", 12), command=self.pause)
+        load_button.place(x=100, y=120, anchor="center")
+
+        self.music_file = False
+        self.playing = False
+
+    def load(self):
+        self.music_file = filedialog.askopenfilename()
+        print("Loaded: ", self.music_file)
+        self.play_restart.set("Play")
+
+    def play(self):
+        if self.music_file:
+            mixer.init()
+            mixer.music.load(self.music_file)
+            mixer.music.play()
+            self.playing = False
+            self.play_restart.set("Restart")
+            self.pause_resume.set("Pause")
+
+    def pause(self):
+        if not self.playing:
+            mixer.music.pause()
+            self.playing = True
+            self.pause_resume.set("Resume")
+        else:
+            mixer.music.unpause()
+            self.playing = False
+            self.pause_resume.set("Pause")
+
+    def stop(self):
+        mixer.music.stop()
+
+
+root = Tk()
+Player(root)
+root.mainloop()
