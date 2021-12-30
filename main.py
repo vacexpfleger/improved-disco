c6c80b69035480b59444324d3b823e3f9324f392
Václav Pfleger <58568137+vacexpfleger@users.noreply.github.com>
2021-12-30 14:40:08 +0100

    1.4
    lze přidat víc songu do playlistu, lze skipovat, ukazuje název songu a čas

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ class App(tk.Tk):
         play_image = ImageTk.PhotoImage(Image.open(self.PATH + "/play.png").resize((40, 40)))
         pause_image = ImageTk.PhotoImage(Image.open(self.PATH + "/pause.png").resize((40, 40)))
         stop_image = ImageTk.PhotoImage(Image.open(self.PATH + "/stop.png").resize((40, 40)))
+        next_image = ImageTk.PhotoImage(Image.open(self.PATH + "/next.png").resize((40, 40)))
+        previous_image = ImageTk.PhotoImage(Image.open(self.PATH + "/previous.png").resize((40, 40)))
 
         # frame init
         self.frame_left = customtkinter.CTkFrame(master=self, width=200, height=App.HEIGHT - 40, corner_radius=10)
@@ -40,46 +42,75 @@ class App(tk.Tk):
         # button init
         self.button_1 = customtkinter.CTkButton(master=self.frame_left, image=play_image, text="", command=self.play,
                                                 width=60, height=60, border_width=0, corner_radius=8)
-        self.button_1.place(relx=0.5, y=120, anchor=tk.CENTER)
+        self.button_1.place(relx=0.5, y=50, anchor=tk.CENTER)
 
         self.button_2 = customtkinter.CTkButton(master=self.frame_left, image=pause_image, text="", command=self.pause,
                                                 width=60, height=60, border_width=0, corner_radius=8)
-        self.button_2.place(relx=0.5, y=190, anchor=tk.CENTER)
+        self.button_2.place(relx=0.5, y=120, anchor=tk.CENTER)
 
         self.button_3 = customtkinter.CTkButton(master=self.frame_left, image=stop_image, text="", command=self.stop,
                                                 width=60, height=60, border_width=0, corner_radius=8)
-        self.button_3.place(relx=0.5, y=260, anchor=tk.CENTER)
+        self.button_3.place(relx=0.5, y=190, anchor=tk.CENTER)
+
+        self.button_4 = customtkinter.CTkButton(master=self.frame_left, image=previous_image, text="",
+                                                command=self.previous, width=60, height=60, border_width=0,
+                                                corner_radius=8)
+        self.button_4.place(relx=0.5, y=260, anchor=tk.CENTER)
+
+        self.button_5 = customtkinter.CTkButton(master=self.frame_left, image=next_image, text="",
+                                                command=self.next, width=60, height=60, border_width=0, corner_radius=8)
+        self.button_5.place(relx=0.5, y=330, anchor=tk.CENTER)
 
         # playlist init
         self.playlist = Listbox(self.frame_right, bg="black", fg="white", width=50)
-        self.playlist.place(relx=0.5, y=100, anchor=tk.CENTER)
+        self.playlist.place(relx=0.5, y=120, anchor=tk.CENTER)
+
+        # status bar init
+        self.status_bar = Label(self.frame_right, text="", bd=1, relief=GROOVE)
+        self.status_bar.place(relx=0.5, y=215, anchor=tk.CENTER)
+        self.status_bar.place_forget()
 
         # menu init
         self.player_menu = Menu(self)
         self.config(menu=self.player_menu)
 
         self.add_song_menu = Menu(self.player_menu)
-        self.player_menu.add_cascade(label="Add Songs", menu=self.add_song_menu)
-        self.add_song_menu.add_command(label="Add One Song", command=self.add_song)
+        self.player_menu.add_cascade(label="File", menu=self.add_song_menu)
+        self.add_song_menu.add_command(label="Add", command=self.add_songs)
+        self.add_song_menu.add_separator()
+        self.add_song_menu.add_command(label="Exit", command=quit)
 
         self.music_file = None
+        self.songs_list = []
         self.playing = False
 
-    def add_song(self):
-        song = filedialog.askopenfilename(title="Choose A Song", filetypes=(("mp3 Files", "*.mp3"), ))
-        self.playlist.insert(END, os.path.basename(song))
-
-    def load(self):
-        self.music_file = filedialog.askopenfilename()
-        print("Loaded: ", self.music_file)
+    def add_songs(self):
+        songs = filedialog.askopenfilenames(title="Choose A Song", filetypes=(("mp3 Files", "*.mp3"), ))
+        for song in songs:
+            split_songs = list(os.path.split(song))  # split song path into two separate parts (directory, file)
+            self.songs_list.append(split_songs)
+            self.playlist.insert(END, split_songs[1])  # adds song name to playlist
+        self.playlist.select_set(0)
 
     def play(self):
-        self.music_file = self.playlist.get(ACTIVE)
+        self.song = self.playlist.get(ACTIVE)
+        song_directory = list(filter(lambda active: active[1] == self.song, self.songs_list))
+        self.music_file = str(song_directory[0][0]) + "/" + str(self.song)
         mixer.music.load(self.music_file)
         mixer.music.play()
-        self.status_bar = Label(self.frame_right, text="", bd=1, relief=GROOVE)
-        self.status_bar.place(relx=0.5, y=205, anchor=tk.CENTER)
         self.play_time()
+
+    def play_alt(self, song_number):
+        self.song = self.playlist.get(song_number)
+        song_directory = list(filter(lambda active: active[1] == self.song, self.songs_list))
+        self.music_file = str(song_directory[0][0]) + "/" + str(self.song)
+        mixer.music.load(self.music_file)
+        mixer.music.play()
+        self.play_time()
+        # highlight the next song in playlist
+        self.playlist.selection_clear(0, END)
+        self.playlist.activate(song_number)
+        self.playlist.selection_set(song_number, last=None)
 
     def pause(self):
         if not self.playing:
@@ -94,14 +125,31 @@ class App(tk.Tk):
         current_time = mixer.music.get_pos() / 1000
         song_duration = time.strftime("%H:%M:%S", time.gmtime(int(MP3(self.music_file).info.length)))
         converted_time = time.strftime("%H:%M:%S", time.gmtime(current_time))
-        final_time = f'{converted_time}/{song_duration}'
+        if self.song:
+            final_time = f'{self.song}\n{converted_time}/{song_duration}'
+        else:
+            mixer.music.stop()
+            final_time = f'End of playlist.'
         self.status_bar.config(text=final_time)
+        self.status_bar.place(relx=0.5, y=215, anchor=tk.CENTER)
         time_update = self.status_bar.after(1000, self.play_time)
+
+    def next(self):
+        # get an index of the next song
+        next_song = self.playlist.curselection()
+        next_song = next_song[0]+1
+        self.play_alt(next_song)
+
+    def previous(self):
+        # get an index of the next song
+        next_song = self.playlist.curselection()
+        next_song = next_song[0] - 1
+        self.play_alt(next_song)
 
     def stop(self):
         mixer.music.stop()
-        self.status_bar.config(text=f'00:00:00/00:00:00')
         self.status_bar.after_cancel(time_update)
+        self.status_bar.config(text=f'{self.song}\n00:00:00/00:00:00')
 
     def startapp(self):
         self.mainloop()
